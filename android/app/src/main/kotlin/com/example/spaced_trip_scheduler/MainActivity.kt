5ed4f0a0941264232273c639280ecf005402721a
Nony Bright <nonybrighto@gmail.com>
2021-08-18 23:49:54 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/spaced_trip_scheduler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spaced_trip_scheduler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xff131313);
